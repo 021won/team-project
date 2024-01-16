f9159ba6caba74f7615d15ae1a9861b331b90aa4
DESKTOP-PTUNS30\tj-bu-702-09 <admin>
2024-01-16 14:02:18 +0900

    result 페이지 수정

--- result.php
+++ result.php
@@ -2,12 +2,12 @@
 
 <section class="result-section">
   <figure>
-    <img class="dog-img" src="./img/말티즈.png" alt="">
+    <img class="dog-img" src="./img/123456.png" alt="">
   </figure>
 
   <form class="result-form">
-    <a href="./index.php"><i class="fa-solid fa-rotate-left"></i> 다시하기</a>
-    <a href="./img/말티즈.png" target="_blank"><i class="fa-solid fa-download"></i> 저장하기</a>
+    <a class="reset" href="./index.php"><i class="fa-solid fa-rotate-left"></i> 다시하기</a>
+    <a class="save" href="./img/말티즈.png" target="_blank"><i class="fa-solid fa-download"></i> 저장하기</a>
   </form>
 </section>
 
