cf88acdac28e6f2fab7ad241ee89ba9616305520
DESKTOP-PTUNS30\tj-bu-702-09 <admin>
2024-01-18 14:18:44 +0900

    hover 효과 수정

--- result.php
+++ result.php
@@ -7,7 +7,7 @@
 
   <form class="result-form">
     <a class="reset" href="./index.php"><i class="fa-solid fa-rotate-left"></i> 다시하기</a>
-    <a class="save" href="./img/말티즈.png" target="_blank"><i class="fa-solid fa-download"></i> 저장하기</a>
+    <a class="save" href="./img/result/enfj.png" target="_blank"><i class="fa-solid fa-download"></i> 저장하기</a>
   </form>
 </section>
 
